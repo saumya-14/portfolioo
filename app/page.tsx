939dffcc89d0cb8067bebce13ad26a519b63e619
saumya-14 <saumya140904@gmail.com>
2025-01-14 22:14:19 +0530

    making grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import { navItems } from "@/data";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import Grid from "@/components/Grid";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero/>
+        <Grid/>
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ const Hero = () => {
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-center md:tracking-wider mb-4 text-lg  lg:text-2xl">
             Hi! I&apos;m Saumya Shrivastava, a Web Developer based in India.
           </p>
 
